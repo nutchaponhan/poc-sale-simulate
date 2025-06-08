09abdf0e37e9a9a16b66c7629a4d681d020d938a
Nutchapon <nutchapon@bluebiks-MacBook-Pro.local>
2025-06-08 21:48:03 +0700

    fix controller

--- src/modules/premium/premium.controller.ts
+++ src/modules/premium/premium.controller.ts
@@ -2,14 +2,36 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import * as path from 'path';
 
-import { PremiumProcessor } from '../../lib/main';
-import { BetterSQLiteAdapter } from 'src/utility/sqlite.adapter';
-import { ProspectInput } from 'src/lib/core/prospect.input';
+import { BetterSQLiteAdapter, DB } from 'src/utility/sqlite.adapter';
+import { Processor } from '../../lib/main';
+import { IPlanInput, IProspectInput, IRiderInput } from '../../lib/interface';
 
 interface PremiumCalculationRequest {
-  prospect: ProspectInput;
-  plans: Array<any>;
-  riders: Array<any>;
+  prospect: {
+    gender: 'M' | 'F';
+    birthDate: string;
+    occgroup: 2;
+    occupationType: '2';
+    paymentMode: 4;
+  };
+  plan: {
+    code: string;
+    premium?: number;
+    assure?: number;
+    previous?: {
+      premium?: number;
+      assure?: number;
+    };
+  };
+  rider: Array<{
+    code: string;
+    premium?: number;
+    assure?: number;
+    previous?: {
+      premium?: number;
+      assure?: number;
+    };
+  }>;
 }
 
 type PremiumCalculationResponse = any;
@@ -25,22 +47,55 @@ export class PremiumController {
     status: 200,
     description: 'Premium calculation successful',
   })
-  calculatePremium(
+  async calculatePremium(
     @Body() request: PremiumCalculationRequest,
-  ): PremiumCalculationResponse {
+  ): Promise<PremiumCalculationResponse> {
     const storagePath = path.join(process.cwd(), 'storage.db');
 
-    // build processors
-    const premiumProcessor = new PremiumProcessor();
+    // build processor
+    const processor = new Processor();
 
-    // load storage
-    premiumProcessor.load(new BetterSQLiteAdapter(storagePath));
+    const sqliteDbAdapter = new BetterSQLiteAdapter(storagePath);
+    const db = DB.getInstance(sqliteDbAdapter);
+
+    await db.initializeTables();
+
+    const prospectInput: IProspectInput = {
+      gender: request.prospect.gender,
+      birthDate: request.prospect.birthDate,
+      occupationType: request.prospect.occupationType,
+      paymentMode: request.prospect.paymentMode,
+    };
+
+    const planInput: IPlanInput = {
+      ...request.plan,
+      productConfig: await db.get(request.plan.code, 'PRODUCT'),
+      rateConfig: await db.get(request.plan.code, 'RATE'),
+      previous: request.plan.previous,
+    };
+
+    const ridersInput = await request.rider.reduce<
+      Promise<Record<string, IRiderInput>>
+    >(async (riders, r) => {
+      const riderProductConfig = await db.get(r.code, 'PRODUCT');
+      const riderRateConfig = await db.get(r.code, 'RATE');
+
+      return {
+        ...(await riders),
+        [r.code]: {
+          ...r,
+          productConfig: riderProductConfig,
+          rateConfig: riderRateConfig,
+          previous: r.previous,
+        },
+      };
+    }, Promise.resolve({}));
 
     // calculate premium
-    const result = premiumProcessor.process({
-      prospect: request.prospect,
-      plans: request.plans,
-      riders: request.riders,
+    const result = processor.process({
+      prospect: prospectInput,
+      plan: planInput,
+      rider: ridersInput,
     });
 
     return result;
--- src/utility/sqlite.adapter.ts
+++ src/utility/sqlite.adapter.ts
@@ -1,5 +1,10 @@
 import * as Database from 'better-sqlite3';
-import { ISQLiteAdapter } from '../lib/utility/db';
+
+interface ISQLiteAdapter {
+  initializeTables(): Promise<void>;
+  get(...params: any[]): Promise<any>;
+  close(): Promise<void>;
+}
 
 export class BetterSQLiteAdapter implements ISQLiteAdapter {
   private db: Database.Database;
@@ -9,15 +14,15 @@ export class BetterSQLiteAdapter implements ISQLiteAdapter {
     this.initializeTables();
   }
 
-  initializeTables(): void {
-    this.db.exec(
+  async initializeTables(): Promise<void> {
+    await this.db.exec(
       'CREATE TABLE IF NOT EXISTS config (code TEXT NOT NULL, type TEXT NOT NULL, value TEXT, PRIMARY KEY (code, type))',
     );
   }
 
-  get(...params: any[]): any {
+  async get(...params: any[]): Promise<any> {
     const [code, type] = params;
-    const stmt = this.db.prepare(
+    const stmt = await this.db.prepare(
       'SELECT value FROM config WHERE code = ? AND type = ?',
     );
 
@@ -32,7 +37,39 @@ export class BetterSQLiteAdapter implements ISQLiteAdapter {
     }
   }
 
-  close(): void {
-    this.db.close();
+  async close(): Promise<void> {
+    await this.db.close();
+  }
+}
+
+export class DB {
+  private static instance: DB | null = null;
+  private dbAdapter: ISQLiteAdapter;
+
+  private constructor(dbAdapter: ISQLiteAdapter) {
+    this.dbAdapter = dbAdapter;
+  }
+
+  static getInstance(adapter?: ISQLiteAdapter): DB {
+    if (!DB.instance) {
+      if (adapter) {
+        DB.instance = new DB(adapter);
+      } else {
+        throw new Error('Adapter is required');
+      }
+    }
+    return DB.instance;
+  }
+
+  async initializeTables(): Promise<void> {
+    await this.dbAdapter.initializeTables();
+  }
+
+  async get(...params: any[]): Promise<any> {
+    return this.dbAdapter.get(...params);
+  }
+
+  async close(): Promise<void> {
+    return this.dbAdapter.close();
   }
 }
